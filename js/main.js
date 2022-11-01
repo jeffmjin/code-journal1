050e3d1e5b601ced91d02260ce286f1272545cb5
Jeff Jin <jeffmjin@gmail.com>
2022-11-01 21:29:39 +0000

    Put the form's input values into a new object.

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
+/* global data */
+
 var $url = document.querySelector('#url');
 var $photo = document.querySelector('#image');
 var $form = document.querySelector('#form');
 
 $url.addEventListener('input', handleInput);
+$form.addEventListener('submit', handleSubmit);
 
 function handleInput(event) {
   if ($photo === '') {
@@ -10,4 +13,14 @@ function handleInput(event) {
   } else {
     $photo.setAttribute('src', $form.elements.url.value);
   }
+}
+
+function handleSubmit(event) {
+
+  var formData = {
+    title: $form.elements.name.value,
+    url: $form.elements.url.value,
+    notes: $form.elements.notes.value
+  };
+
 }
